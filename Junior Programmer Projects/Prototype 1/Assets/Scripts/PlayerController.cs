116c4ba177f4c66dc29715897cfaa9105802e032
KayBeeC <ka3b3c@gmail.com>
2022-07-23 16:26:31 +0100

    Prototype 1 Continuation
    Completed Tutorial 1.4, next up Challenge 1

--- Junior Programmer Projects/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Junior Programmer Projects/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed = 5.0f;
+    private float speed = 5.0f;
+    private float turnspeed = 25.0f;
+    private float horizontalinput;
+    private float forwardinput;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalinput = Input.GetAxis("Horizontal");
+        forwardinput = Input.GetAxis("Vertical");
+
         //Moves the vehicle forward
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*forwardinput);
+        //Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnspeed * horizontalinput * Time.deltaTime);
+        //Might need to delete this line
+        //transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horizontalinput);
     }
 }
